04d229021ade23e299802cea6ff88eae2f51a42d
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-31 11:35:59 +0200

    Do not migrate for frontend
    Copy rest of the deploy scripts

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,4 @@ set :stage, :staging
 set :ip, "162.13.156.110"
 set :domain_name, "scheme-finder-frontend.dev.bitzesty.com"
 
-server fetch(:ip), user: fetch(:user), roles: %w(app web db)
+server fetch(:ip), user: fetch(:user), roles: %w(app web)
--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -2,4 +2,4 @@ set :stage, :vagrant
 set :ip, "10.0.100.15"
 set :domain_name, "scheme-finder-frontend.app"
 
-server fetch(:ip), user: fetch(:user), roles: %w(app web db)
+server fetch(:ip), user: fetch(:user), roles: %w(app web)
