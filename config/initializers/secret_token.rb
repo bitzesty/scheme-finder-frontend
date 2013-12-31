70a39b44ac4d9fe4f547b2e47d69cec802ad759d
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-31 12:25:58 +0200

    Use cookie secret key from ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SchemeFinderFrontend::Application.config.secret_key_base = 'f8a14991a276e1e8e70925031c5d13826838b8b7f7d4df47b36ca7f7c66d22a6be0e88cdf66f68e0c44931c7401bf301f1162066fb3d618f26eebb0ad854976c'
+SchemeFinderFrontend::Application.config.secret_key_base = ENV['COOKIE_SECRET_KEY'] || 'development'
