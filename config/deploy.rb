d35a8d4d8b3fd576f181fb24c56b92c7400be158
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-31 10:56:12 +0200

    Add deploy configuration

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "scheme-finder-frontend"
+set :repo_url, "git@github.com:bitzesty/#{fetch(:application)}.git"
+set :user, "schemer"
+
+set :hipchat_token, ENV["HIPCHAT_TOKEN"]
+set :hipchat_room_name, "BIS"
+set :hipchat_announce, false
+set :hipchat_color, "yellow"
+set :hipchat_success_color, "green"
+set :hipchat_failed_color, "red"
+
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :scm, :git
+
+set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
+set :branch, ENV["BRANCH"] || "master"
+set :use_sudo, false
+
+set :format, :pretty
+set :log_level, :info
+set :pty, true
+
+set :linked_files, %w{.env}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle
+                     public/system}
+
+set :default_env, {
+  "LANG" => "en_US.UTF-8",
+  "PATH" => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH",
+}
+set :keep_releases, 5
+
+set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
+fetch(:default_env).merge!(rails_env: fetch(:stage))
---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,5 @@
+set :stage, :vagrant
+set :ip, "10.0.100.15"
+set :domain_name, "scheme-finder-frontend.app"
+
+server fetch(:ip), user: fetch(:user), roles: %w(app web db)
