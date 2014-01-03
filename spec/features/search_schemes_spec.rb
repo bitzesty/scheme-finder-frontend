55cd6ea6790e28473b1c8e33cce3c90a30c981bf
Karolis Astrauka <astrauka@gmail.com>
2014-01-03 11:37:37 +0200

    update vcr cassettes

--- spec/features/search_schemes_spec.rb
+++ spec/features/search_schemes_spec.rb
@@ -1,16 +1,21 @@
 require "spec_helper"
 
 describe "Scheme search" do
-  include SchemeSteps
-
-  before do
-    %w(locations sectors commitment_lengths activities company_sizes age_ranges).each do |entity|
-      stub_entity_response entity
+  it "uses api for searching schemes" do
+    VCR.use_cassette("schemes_search") do
+      filter_by_sector("Auto")
+      expect_to_see "Automotives"
     end
   end
 
-  it "uses api for searching schemes" do
-    filter_by_sector("Auto")
-    expect_to_see "Automotives"
+  private
+
+  # Command
+  #
+  # filters schemes by sector
+  def filter_by_sector(sector)
+    ensure_on schemes_path
+    select sector, from: "search_sectors"
+    click_on "submit_btn"
   end
 end
