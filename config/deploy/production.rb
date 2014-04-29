1c121ab8e98dc2bfeb7d092257a01d96a1f8ea0f
Karolis Astrauka <astrauka@gmail.com>
2014-04-29 12:10:19 +0300

    update production host name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ set :rails_env, :production
 set :branch, ENV["BRANCH"] || "production"
 
 set :ip, "89.200.142.59"
-set :domain_name, "makeitingreatbritain.bis.gov.uk"
+set :domain_name, "bistooltest.educationandemployers.org"
 
 server fetch(:ip), user: fetch(:user), roles: %w(app web)
