68686e3fb30f8c14eed99c24f30f263d3a199239
Matthew Ford <matt@bitzesty.com>
2014-05-02 18:14:22 +0100

    Use ENV Variable for getsentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,5 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.dsn = 'https://166e257bdc0a4464b4aa03076d0a0cda:43cdd2f177b04fe3b7723422bbd9ea1b@app.getsentry.com/23443'
+  config.dsn = ENV['GETSENTRY']
 end
