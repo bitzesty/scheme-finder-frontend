7e9c7ad8fd739a95e9a851a243f83d96600624c9
Matthew Ford <matt@bitzesty.com>
2015-05-11 12:03:18 +0100

    update domain name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ set :rails_env, :production
 set :branch, ENV["BRANCH"] || "production"
 
 set :ip, "89.200.142.59"
-set :domain_name, "bistooltest.educationandemployers.org"
+set :domain_name, "growingambitions.educationandemployers.org"
 
 server fetch(:ip), user: fetch(:user), roles: %w(app web)
